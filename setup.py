832ae97c782777eb9e82311c2e2f72afd84e7487
Chris Strutton <chris@rodandfly.ca>
2022-10-21 10:44:30 -0400

    moved to PyCharm

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pms',
+    version='0.1',
+    packages=['pms', 'barcode_verify', 'barcode_verify.migrations'],
+    url='https://github.com/Stackpole-International-Stratford/pms.git',
+    license='',
+    author='Chris.Strutton',
+    author_email='chris@rodandfly.ca',
+    description='A project to bring Trakberry to Django 4+ and Python 3.9+'
+)

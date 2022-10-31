32d2eec1961dd1e90fd90d9841d5a59cad423d23
Chris Strutton <chris@rodandfly.ca>
2022-10-31 15:08:31 -0400

    updated urls

--- prod_query/urls.py
+++ prod_query/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('shift-line', views.shift_line, name='shift_line'),
+    path('shift-line', views.shift_line, name='shift-line'),
 ]

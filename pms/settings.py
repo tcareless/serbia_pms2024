5ae07f1e5b17854def7cecc8d9bf380fc38a2091
Chris Strutton <chris@rodandfly.ca>
2023-01-29 11:34:24 -0500

    move static files to common_static

--- pms/settings.py
+++ pms/settings.py
@@ -161,9 +161,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static',]
-STATIC_ROOT = os.path.join(BASE_DIR,'nginx_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'common_static'),
+]
 
 
 

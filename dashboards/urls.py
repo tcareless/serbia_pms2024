9686b33345b94050c4aa3755eb24bcfc430a5f6e
Chris Strutton <chris@rodandfly.ca>
2023-02-03 15:39:08 -0500

    Added new template for 931 mobile

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('cell_track_9341/', views.cell_track_9341, {'template': 'cell_track_9341.html'}, name='track9341'),
     path('cell_track_9341_TV/', views.cell_track_9341, {'template': 'cell_track_9341.html'}, name='track9341_TV'),
     path('cell_track_9341_mobile/', views.cell_track_9341, {'template': 'cell_track_9341_mobile.html'}, name='track9341_mobile'),
+    path('cell_track_9341_mobile2/', views.cell_track_9341, {'template': 'cell_track_9341_mobile2.html'}, name='track9341_mobile'),
+
 
     path('1467/', views.cell_track_1467, {'template': 'cell_track_1467.html'}, name='1467'),
     path('cell_track_1467/', views.cell_track_1467, {'template': 'cell_track_1467.html'}, name='track1467'),

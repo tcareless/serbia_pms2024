8baaf6af18fb38ea8232befecef7e92166fbcaeb
Chris Strutton <chris@rodandfly.ca>
2023-02-20 15:55:48 -0500

    cleanup

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-git checkout main
-git pull
-
-git checkout production
-git merge main
-
-docker-compose -f docker-compose-deploy.yml up -d --build
--- scripts/redeploy.sh
+++
@@ -1,3 +0,0 @@
-reload_nginx() {  
-  docker exec pms_proxy_1 /usr/sbin/nginx -s reload  
-}

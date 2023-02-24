ec060df27fe486b4b2ce1551a07bac8cda194b60
Chris Strutton <chris@rodandfly.ca>
2023-02-24 08:21:55 -0500

    adding nginx-proxy to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -46,3 +46,12 @@ if [ $( docker ps -a | grep pms_temp | wc -l ) -gt 0 ]; then
   docker container stop pms_temp
   docker container rm pms_temp
 fi
+
+#if [ $( docker ps -a | grep nginx-proxy | wc -l ) -eq 0 ]; then
+#  echo "Starting Nginx Proxy container"
+#  docker run -d -p 80:80 \
+#    -v /var/run/docker.sock:/tmp/docker.sock:ro \
+#    -t nginx-proxy \
+#    docker.io/nginxproxy/nginx-proxy:latest
+#fi
+

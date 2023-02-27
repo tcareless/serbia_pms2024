1993c54d3d32e8de5a389c33bb386ffe0aa30061
Chris Strutton <chris@rodandfly.ca>
2023-02-27 10:00:34 -0500

    update uwsgi to run on 80

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py collectstatic --noinput
 
 # uwsgi --socket :8000 --master --enable-threads --module pms.wsgi 
 
-uwsgi --http 0.0.0.0:80 --master --module pms.wsgi --processes 4 --enable-threads
+uwsgi --http 0.0.0.0:80 --master --module pms.wsgi --processes 4 

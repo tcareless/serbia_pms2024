aa5215da8f178c68533aefa518228ad0f27d88fc
Chris Strutton <chris@rodandfly.ca>
2023-03-01 15:41:12 -0500

    reworking deploy scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+set -e
+version="0.1.7-5"
+
+git checkout production
+
+docker build -t pms:$version .
+
+docker container rm --force pms &>/dev/null && echo 'Removed old container'
+
+docker run -d -p 80:80 --name pms --restart=unless-stopped pms:$version

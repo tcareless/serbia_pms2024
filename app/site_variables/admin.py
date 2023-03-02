703480a74b9223def45ef20c05657349a4a53d3f
Chris Strutton <chris@rodandfly.ca>
2023-03-02 17:51:48 -0500

    make variables and editable list in the admin

--- app/site_variables/admin.py
+++ app/site_variables/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import SiteVariableModel
 
-admin.site.register(SiteVariableModel)
+class SiteVariableAdmin(admin.ModelAdmin):
+    list_display = ("variable_name", "variable_value")
+    list_editable = ("variable_value",)
+
+admin.site.register(SiteVariableModel, SiteVariableAdmin)

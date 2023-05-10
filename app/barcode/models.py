1a182d91db5d98841e47260809553f732a75a8a4
Chris Strutton <chris@rodandfly.ca>
2023-05-10 16:16:27 -0400

    increase part number field width in barcode model

--- app/barcode/models.py
+++ app/barcode/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
 class LaserMark(models.Model):
-    part_number = models.CharField(max_length=7)
+    part_number = models.CharField(max_length=20)
     bar_code = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     grade = models.CharField(max_length=1, null=True)
@@ -16,26 +18,27 @@ class LaserMark(models.Model):
 
 
 class LaserMarkMeasurementData(models.Model):
-    laser_mark = models.OneToOneField(LaserMark, on_delete=models.CASCADE, primary_key=True)
+    laser_mark = models.OneToOneField(
+        LaserMark, on_delete=models.CASCADE, primary_key=True)
     measurement_data = models.TextField()
 
     class Meta:
         ordering = ['laser_mark']
 
     def __str__(self):
-       return self.laser_mark.bar_code
+        return self.laser_mark.bar_code
 
 
 class LaserMarkDuplicateScan(models.Model):
-    laser_mark = models.OneToOneField(LaserMark, on_delete=models.CASCADE, primary_key=True)
+    laser_mark = models.OneToOneField(
+        LaserMark, on_delete=models.CASCADE, primary_key=True)
     scanned_at = models.DateTimeField(auto_now_add=True)
-
 
     class Meta:
         ordering = ['scanned_at']
 
     def __str__(self):
-       return f'{self.laser_mark.bar_code} scanned at {self.scanned_at}'
+        return f'{self.laser_mark.bar_code} scanned at {self.scanned_at}'
 
 
 class BarCodePUN(models.Model):
@@ -43,7 +46,7 @@ class BarCodePUN(models.Model):
     part_number = models.CharField(max_length=50)
     regex = models.CharField(max_length=120)
     active = models.BooleanField()
-    
+
     class Meta:
         ordering = ['part_number']
 

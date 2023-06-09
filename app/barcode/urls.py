8cb39ade99e2ebcd47964b7880d96aaad8f7f9a3
Chris Strutton <chris@rodandfly.ca>
2023-06-09 09:08:15 -0400

    add veriy endpoint for ajax check

--- app/barcode/urls.py
+++ app/barcode/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', RedirectView.as_view(pattern_name='duplicate-scan')),
     path('duplicate', views.duplicate_scan, name='duplicate-scan'),
-    path('duplicate_check', views.duplicate_scan_check, name='duplicate-scan-check'),
+    path('duplicate_check', views.duplicate_scan_check,
+         name='duplicate-scan-check'),
+    path('verify', views.verify_barcode, name='verify'),
     # path('quality', views.quality_scan, name='quality-scan'),
 ]

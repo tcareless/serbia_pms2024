30719a46efc77610b09c5f014540ea4b1618a62d
Gerald Cardenas <gcardenas@stackpole.ca>
2023-07-12 13:17:54 -0400

    basic version copied templates/views, added buttons, no modifications to code in use

--- app/barcode/urls.py
+++ app/barcode/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', RedirectView.as_view(pattern_name='duplicate-scan')),
+    path('gfx/scan', views.gfx_scan_view, name='gfx-scan'),
+    path('gfx/batch', views.gfx_batch_view, name='gfx-batch'),
+    path('gfx/check', views.gfx_check_view, name='gfx-check'),
     path('duplicate', views.duplicate_scan, name='duplicate-scan'),
     path('duplicate_check', views.duplicate_scan_check,
          name='duplicate-scan-check'),
--- app/barcode/views.py
+++ app/barcode/views.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.http import JsonResponse
+from django.http import HttpResponseRedirect
 
 from barcode.forms import BarcodeScanForm, BatchBarcodeScanForm
 from barcode.models import LaserMark, LaserMarkDuplicateScan, BarCodePUN
@@ -189,7 +189,7 @@ def duplicate_scan(request):
     context['active_part'] = current_part_id
     context['part_select_options'] = select_part_options
     context['timer'] = f'{toc-tic:.3f}'
-
+    
     return render(request, 'barcode/dup_scan.html', context=context)
 
 
@@ -352,3 +352,322 @@ def duplicate_scan_check(request):
     context['timer'] = f'{toc-tic:.3f}'
 
     return render(request, 'barcode/dup_scan.html', context=context)
+
+def scan_helper(request):
+    
+    context = {}
+    tic = time.time()
+    # get data from session
+    running_count = int(request.session.get('RunningCount', '0'))
+    last_part_id = request.session.get('LastPartID', '0')
+    # initialize current_part_id if no barcode submitted
+    current_part_id = last_part_id
+
+    # set lm to None to prevent error
+    lm = None
+
+    select_part_options = BarCodePUN.objects.filter(
+        active=True).order_by('name').values()
+
+    if request.method == 'GET':
+        # clear the form
+        form = BarcodeScanForm()
+
+    if request.method == 'POST':
+
+        if 'switch-check' in request.POST:
+            context['active_part'] = current_part_id
+            context['target'] = 'barcode/gfx_check.html'
+            return redirect("gfx-check")
+
+        if 'switch-batch' in request.POST:
+            context['active_part'] = current_part_id
+            context['target'] = 'barcode/gfx_batch.html'
+            return redirect("gfx-batch")
+
+        if 'set_count' in request.POST:
+            messages.add_message(request, messages.INFO, 'Count reset.')
+            running_count = request.POST.get('count', 0) or 0
+            running_count = int(running_count)
+            form = BarcodeScanForm()
+
+        elif 'btnsubmit' in request.POST:
+            form = BarcodeScanForm(request.POST)
+
+            if form.is_valid():
+                # get or create a laser-mark for the scanned code
+                barcode = form.cleaned_data.get('barcode')
+
+                current_part_id = int(request.POST.get('part_select', '0'))
+
+                current_part_PUN = BarCodePUN.objects.get(id=current_part_id)
+
+                if not re.search(current_part_PUN.regex, barcode):
+                    print('Malformed Barcode')
+                    # malformed barcode
+                    context['scanned_barcode'] = barcode
+                    context['part_number'] = current_part_PUN.part_number
+                    context['expected_format'] = current_part_PUN.regex
+                    context['target'] = 'barcode/malformed.html'
+                    return request, context
+
+                # does barcode exist?
+                lm, created = LaserMark.objects.get_or_create(bar_code=barcode)
+                if created:
+                    # laser mark does not exist in db.  Need to create it.
+                    lm.part_number = current_part_PUN.part_number
+                    lm.save()
+
+                # has barcode been duplicate scanned?
+                if lm.grade not in ('A', 'B', 'C'):
+                    context['scanned_barcode'] = barcode
+                    context['part_number'] = lm.part_number
+                    context['grade'] = lm.grade
+                    context['target'] = "barcode/failed_grade.html"
+                    return request, context
+
+
+                # has barcode been duplicate scanned?
+                dup_scan, created = LaserMarkDuplicateScan.objects.get_or_create(
+                    laser_mark=lm)
+                if not created:
+                    # barcode has already been scanned
+                    context['scanned_barcode'] = barcode
+                    context['part_number'] = lm.part_number
+                    context['duplicate_scan_at'] = dup_scan.scanned_at
+                    context['target'] = "barcode/dup_found.html"
+                    return request, context
+
+                else:
+                    # barcode has not been scanned previously
+                    dup_scan.save()
+                    # pass data to the template
+                    messages.add_message(
+                        request, messages.SUCCESS, 'Valid Barcode Scanned')
+                    running_count += 1
+
+                    # use the session to track the last successfully scanned part type
+                    # to detect part type changes and reset the count
+                    request.session['LastPartID'] = current_part_id
+
+                    # clear the form data for the next one
+                    form = BarcodeScanForm()
+
+                print(f'{current_part_PUN.part_number}:{running_count}, {barcode}')
+        else:
+            current_part_id = int(request.POST.get('part_select', '0'))
+            running_count = 0
+            form = BarcodeScanForm()
+
+    toc = time.time()
+    # use the session to maintain a running count of parts per user
+    request.session['RunningCount'] = running_count
+
+    # context['last_part_status'] = last_part_status
+    context['form'] = form
+    context['running_count'] = running_count
+    context['title'] = 'Duplicate Scan'
+    context['scan_check'] = False
+    context['active_part'] = current_part_id
+    context['part_select_options'] = select_part_options
+    context['timer'] = f'{toc-tic:.3f}'
+    context['target'] = 'barcode/gfx_scan.html'
+    
+    return request, context
+
+def batch_helper(request):
+    context = {}
+    tic = time.time()
+    # get data from session
+    last_part_id = request.session.get('LastPartID', '0')
+    current_part_id = last_part_id
+
+    select_part_options = BarCodePUN.objects.filter(
+        active=True).order_by('name').values()
+    if current_part_id == '0':
+        if select_part_options.first():
+            current_part_id = select_part_options.first()['id']
+    current_part_PUN = BarCodePUN.objects.get(id=current_part_id)
+
+    if request.method == 'GET':
+        # clear the form
+        form = BatchBarcodeScanForm()
+
+    if request.method == 'POST':
+        barcodes = request.POST.get('barcodes')
+
+        if 'switch-single' in request.POST:
+            context['active_part'] = current_part_id
+            context['target'] = 'barcode/gfx_scan.html'
+            return redirect("gfx-scan")
+
+        if 'switch-check' in request.POST:
+            context['active_part'] = current_part_id
+            context['target'] = 'barcode/gfx_check.html'
+            return redirect("gfx-check")
+
+        if len(barcodes):
+            form = BatchBarcodeScanForm(request.POST)
+
+            if form.is_valid():
+                barcodes = form.cleaned_data.get('barcodes').split("\r\n")
+
+                posted_part_id = int(request.POST.get('part_select', '0'))
+                if posted_part_id:
+                    current_part_id = posted_part_id
+                processed_barcodes = []
+                for barcode in barcodes:
+                    pass #XYZ
+                    # get or create a laser-mark for the scanned code
+                    # processed_barcodes.append(
+                        # verify_barcode(current_part_id, barcode)) #XYZ
+                    # print(f'{current_part_PUN.part_number}:{barcode}')
+
+                for barcode in processed_barcodes:
+
+                    # Malformed Barcode
+                    if barcode['status'] == 'malformed':
+                        print('Malformed Barcode')
+                        context['scanned_barcode'] = barcode
+                        context['part_number'] = current_part_PUN.part_number
+                        context['expected_format'] = current_part_PUN.regex
+                        context['target'] = "barcode/malformed.html"
+                        return request, context
+
+                    # verify the barcode has a passing grade on file?
+                    if barcode['status'] == 'failed_grade':
+                        context['scanned_barcode'] = barcode
+                        context['part_number'] = current_part_PUN.part_number
+                        context['grade'] = barcode['grade']
+                        context['target'] = "barcode/failed_grade.html"
+                        return request, context
+
+                    # barcode has already been scanned
+                    if barcode['status'] == 'duplicate':
+                        context['scanned_barcode'] = barcode['barcode']
+                        context['part_number'] = barcode['part_number']
+                        context['duplicate_scan_at'] = barcode['scanned_at']
+                        context['target'] = "barcode/dup_found.html"
+                        return request, context
+
+        else:
+            current_part_id = int(request.POST.get('part_select', '0'))
+            if current_part_id == '0':
+                if select_part_options.first():
+                    current_part_id = select_part_options.first()['id']
+            form = BatchBarcodeScanForm()
+
+    context['form'] = form
+    context['title'] = 'Batch Duplicate Scan'
+    context['active_part'] = current_part_id
+    context['part_select_options'] = select_part_options
+    current_part_PUN = BarCodePUN.objects.get(id=current_part_id)
+    context['active_part_prefix'] = current_part_PUN.regex[1:5]
+    context['parts_per_tray'] = current_part_PUN.parts_per_tray
+    context['target'] = 'barcode/gfx_batch.html'
+
+    request.session['LastPartID'] = current_part_id
+
+    toc = time.time()
+    context['timer'] = f'{toc-tic:.3f}'
+
+    return request, context
+
+def check_helper(request):
+    context = {}
+    tic = time.time()
+
+    current_part_id = request.session.get('LastPart', '0')
+
+    select_part_options = BarCodePUN.objects.filter(
+        active=True).order_by('name').values()
+
+    if request.method == 'GET':
+        # clear the form
+        form = BarcodeScanForm()
+
+    if request.method == 'POST':
+        if 'switch-single' in request.POST:
+            context['active_part'] = current_part_id
+            context['target'] = 'barcode/gfx_scan.html'
+            return redirect("gfx-scan")
+
+        if 'switch-batch' in request.POST:
+            context['active_part'] = current_part_id
+            context['target'] = 'barcode/gfx_batch.html'
+            return redirect("gfx-batch")
+
+        if 'btnsubmit' in request.POST:
+
+            form = BarcodeScanForm(request.POST)
+
+            if form.is_valid():
+
+                barcode = form.cleaned_data.get('barcode')
+
+                current_part_id = int(request.POST.get('part_select', '0'))
+
+                current_part_PUN = BarCodePUN.objects.get(id=current_part_id)
+
+                if not re.search(current_part_PUN.regex, barcode):
+                    # malformed barcode
+                    context['scanned_barcode'] = barcode
+                    context['part_number'] = current_part_PUN.part_number
+                    context['expected_format'] = current_part_PUN.regex
+                    context['target'] = "barcode/malformed.html"
+                    return request, context
+                
+                # does barcode exist?
+                lm, created = LaserMark.objects.get_or_create(bar_code=barcode)
+                if created:
+                    # laser mark does not exist in db.  Need to create it.
+                    lm.part_number = current_part_PUN.part_number
+                    lm.save()
+
+                # has barcode been duplicate scanned?
+                dup_scan, created = LaserMarkDuplicateScan.objects.get_or_create(
+                    laser_mark=lm)
+                if created:
+                    # barcode has not been scanned previously
+                    messages.add_message(
+                        request, messages.ERROR, 'Barcode Not Previously Scanned')
+                    dup_scan.delete()
+                    form = BarcodeScanForm()
+                else:
+                    # barcode has already been scanned
+                    messages.add_message(
+                        request, messages.SUCCESS, f'Barcode Previously Scanned at {dup_scan.scanned_at}')
+                    context['status_last'] = 'good'
+                    form = BarcodeScanForm()
+        else:
+            current_part_id = int(request.POST.get('part_select', '0'))
+            form = BarcodeScanForm()
+
+    toc = time.time()
+    request.session['LastPart'] = current_part_id
+
+    context['form'] = form
+    context['title'] = 'Duplicate Scan Check'
+    context['scan_check'] = True
+    context['active_part'] = int(current_part_id)
+    context['part_select_options'] = select_part_options
+    context['timer'] = f'{toc-tic:.3f}'
+    context['target'] = 'barcode/gfx_check.html'
+
+    return request, context
+
+def gfx_scan_view(request):
+    ret = scan_helper(request)
+    if isinstance(ret, HttpResponseRedirect):
+        return ret
+    return render(ret[0], ret[1]['target'], context=ret[1])
+def gfx_check_view(request):
+    ret = check_helper(request)
+    if isinstance(ret, HttpResponseRedirect):
+        return ret
+    return render(ret[0], ret[1]['target'], context=ret[1])
+def gfx_batch_view(request):
+    ret = batch_helper(request)
+    if isinstance(ret, HttpResponseRedirect):
+        return ret
+    return render(ret[0], ret[1]['target'], context=ret[1])

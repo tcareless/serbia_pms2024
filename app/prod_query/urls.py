4cde70ad2cd89ff3b019737eb8e5feb7d766fa9b
Chris Strutton <chris@rodandfly.ca>
2023-08-18 08:04:04 -0400

    testing fix

--- app/prod_query/urls.py
+++ app/prod_query/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('weekly-prod', views.weekly_prod, name='weekly-prod'),
     path('rejects', views.reject_query, name='rejects'),
     path('cycle-times', views.cycle_times, name='cycle-times'),
-    path('<str:machine>/<int:start_timestamp>/<int:times>/',
+    # path('<str:machine>/<int:start_timestamp>/<int:times>/',
          views.machine_detail, name='machine_detail'),
 ]

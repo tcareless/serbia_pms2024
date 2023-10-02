ce3d7ddfa3eac7ac935ce6297001b501cd6744c8
Sean Kuehl <skuehl@stackpole.com>
2023-10-02 11:35:36 -0400

    added prod_goals

--- app/prod_query/models.py
+++ app/prod_query/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+#id, part, week, year, goal
+
+class Weekly_Production_Goals(models.Model):
+    part_number = models.CharField(max_length=20)
+    week = models.IntegerField()
+    year = models.IntegerField()
+    goal = models.IntegerField()

9cc94299dcb1e3cef38920b43584274c0b2d9cf4
Sean Kuehl <skuehl@stackpole.com>
2023-10-04 15:07:32 -0400

    add better weekly goal description in admin view

--- app/prod_query/models.py
+++ app/prod_query/models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.db import models
 
 # Create your models here.
@@ -10,5 +11,15 @@ class Weekly_Production_Goal(models.Model):
     year = models.IntegerField()
     goal = models.IntegerField()
 
+    def __str__(self):
+        #display weekly_production_goal as something other than 'object(x)' in admin view
+        #part number, week of: day/month/year
+        #day is sunday which based on isocalendar is 7
+        iso_sunday = 7
+        date_for_string = date.fromisocalendar(self.year, self.week, iso_sunday)
+        return f'{self.part_number}, week of: {date_for_string}'
+
     class Meta:
         ordering = ["-year", "-week"]
+
+        

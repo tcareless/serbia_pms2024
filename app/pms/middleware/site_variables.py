87941197489caa7c1a8bb3b779b07a4f88dfa196
Chris Strutton <chris@rodandfly.ca>
2023-11-21 11:46:01 -0500

    ready for production

--- app/pms/middleware/site_variables.py
+++ app/pms/middleware/site_variables.py
@@ -1,31 +1,23 @@
-from django.core.cache import cache
-
 def SiteVariableMiddleware(get_response):
     # One-time configuration and initialization.
     from site_variables.models import SiteVariableModel
+    from django.core.cache import cache
 
     def middleware(request):
-
 
         site_variables = cache.get('site_variables')
         
         if not site_variables:
-        
-
             variables = SiteVariableModel.objects.all()
             site_variables = {}
 
             for variable in variables:
                 site_variables[f'{variable.variable_name}'] = variable.variable_value
 
-        cache.set('site_variables', site_variables, 10)
-
+            cache.set('site_variables', site_variables, 10)
         
         request.site_variables = site_variables
         response = get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
 
         return response
 

1ab4a848a64a5468b0cdc5598534cf24ae642a55
Tyler Careless <tyler444c@gmail.com>
2024-05-10 15:00:28 -0400

    Added tooling path to project urls. No need to touch anything in admin.py for now. Within tooling app, added url pattern to report/, created a basic html to test view

--- app/tooling/urls.py
+++ app/tooling/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from .views import tool_report_form
+
+
+app_name = "tooling"
 
 urlpatterns = [
-    path('', views.view_name, name=''),
+    path('report/', tool_report_form, name='tool_report_form'),
 ]

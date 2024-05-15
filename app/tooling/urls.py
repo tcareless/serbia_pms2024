7d36214abc1e43b3f964d3cfcf79ea6626bba68c
Tyler Careless <tyler444c@gmail.com>
2024-05-15 13:32:55 -0400

    updated my urls.py to include a label page now which will be routed to once the form is submitted

--- app/tooling/urls.py
+++ app/tooling/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from .views import tool_report_form
+from .views import tool_report_form, label_page
 
 
 app_name = "tooling"
 
 urlpatterns = [
     path('report/', tool_report_form, name='tool_report_form'),
+    path('label/', label_page, name='label_page'),
+
 ]

e80e0f7eae8e22bf0aa94711d7f6146bae09eb3d
Tyler Careless <tyler444c@gmail.com>
2024-05-27 15:33:47 -0400

    Variables now working as well

--- app/site_variables/urls.py
+++ app/site_variables/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "variables"
+
 urlpatterns = [
     path('', views.create_view, name='index'),
-
     path('sub-index/', views.sub_index, name='sub-index'),  # New sub-index
-
 ]

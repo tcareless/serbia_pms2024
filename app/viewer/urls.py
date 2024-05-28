72ec6e38f8077fe521a39aba668a2118a76dfa7b
Tyler Careless <tyler444c@gmail.com>
2024-05-28 13:49:17 -0400

    Made the urls viewer/gfx/

--- app/viewer/urls.py
+++ app/viewer/urls.py
@@ -4,9 +4,9 @@ from .views import rabbits_view, production, bypass_status, bypasslog, sub_index
 
 urlpatterns = [
     path('gfx/rabbits/', rabbits_view, name='rabbits_view'),
-    path('production/', production, name='production'),
-    path('bypass/', bypass_status, name='bypass_status'),
-    path('bypasslog/', bypasslog, name='bypasslog'),
+    path('gfx/production/', production, name='production'),
+    path('gfx/bypass/', bypass_status, name='bypass_status'),
+    path('gfx/bypasslog/', bypasslog, name='bypasslog'),
     path('sub-index/', sub_index, name='sub-index'),  # New sub-index
 
 ]

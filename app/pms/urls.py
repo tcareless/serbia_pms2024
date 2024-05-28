c71ceac7cfa404fc3e8d529918aed62cc05aa12a
Tyler Careless <tyler444c@gmail.com>
2024-05-28 11:25:52 -0400

    Moved the view from dashboards to pms for clarity..

--- app/pms/urls.py
+++ app/pms/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from dashboards.views import pms_index_view
+from pms.views import pms_index_view
 
 from django.conf import settings
 from django.conf.urls.static import static

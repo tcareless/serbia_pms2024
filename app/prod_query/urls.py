b52535234add6fe71870afc7c9c0ea928abe0875
Tyler Careless <tyler444c@gmail.com>
2024-06-26 11:17:25 -0400

    Fixed url bug?

--- app/prod_query/urls.py
+++ app/prod_query/urls.py
@@ -11,12 +11,11 @@ urlpatterns = [
     path('weekly-prod', views.weekly_prod, name='weekly-prod'),
     path('rejects', views.reject_query, name='rejects'),
     path('cycle-times', views.cycle_times, name='cycle-times'),
-    path('spm', views.strokes_per_min_graph, name='strokes-per-minute'),
+    path('spm/', views.strokes_per_min_graph, name='strokes-per-minute'),
     path('<str:machine>/<int:start_timestamp>/<int:times>/',
          views.machine_detail, name='machine_detail'),
 
     path('sub-index/', views.sub_index, name='sub-index'),  # New sub-index
     path('shift-totals/', views.shift_totals_view, name='shift-totals'),  # New shift totals URL
-
 
 ]

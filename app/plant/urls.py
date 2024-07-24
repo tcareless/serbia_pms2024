754d6dfa009edceef6cadc7a3d599e3c35cb4211
Tyler Careless <tyler444c@gmail.com>
2024-07-24 13:59:21 -0400

    Made separate crud pages for assets and parts now

--- app/plant/urls.py
+++ app/plant/urls.py
@@ -1,13 +1,23 @@
 # plant/urls.py
 
 from django.urls import path
-from .views import display_setups, create_setupfor, edit_setupfor, delete_setupfor, create_asset, create_part
+from .views import (
+    display_setups, create_setupfor, edit_setupfor, delete_setupfor,
+    display_assets, create_asset, edit_asset, delete_asset,
+    display_parts, create_part, edit_part, delete_part
+)
 
 urlpatterns = [
     path('setups/', display_setups, name='display_setups'),
     path('setups/create/', create_setupfor, name='create_setupfor'),
     path('setups/edit/<int:id>/', edit_setupfor, name='edit_setupfor'),
     path('setups/delete/<int:id>/', delete_setupfor, name='delete_setupfor'),
+    path('assets/', display_assets, name='display_assets'),
     path('assets/create/', create_asset, name='create_asset'),
+    path('assets/edit/<int:id>/', edit_asset, name='edit_asset'),
+    path('assets/delete/<int:id>/', delete_asset, name='delete_asset'),
+    path('parts/', display_parts, name='display_parts'),
     path('parts/create/', create_part, name='create_part'),
+    path('parts/edit/<int:id>/', edit_part, name='edit_part'),
+    path('parts/delete/<int:id>/', delete_part, name='delete_part'),
 ]
--- app/plant/views.py
+++ app/plant/views.py
@@ -36,22 +36,66 @@ def delete_setupfor(request, id):
         return redirect('display_setups')
     return render(request, 'plant/delete_setupfor.html', {'setupfor': setupfor})
 
+def display_assets(request):
+    assets = Asset.objects.all().order_by('-id')
+    return render(request, 'plant/display_assets.html', {'assets': assets})
+
 def create_asset(request):
     if request.method == 'POST':
         form = AssetForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('create_setupfor')
+            return redirect('display_assets')
     else:
         form = AssetForm()
     return render(request, 'plant/asset_form.html', {'form': form, 'title': 'Add New Asset'})
+
+def edit_asset(request, id):
+    asset = get_object_or_404(Asset, id=id)
+    if request.method == 'POST':
+        form = AssetForm(request.POST, instance=asset)
+        if form.is_valid():
+            form.save()
+            return redirect('display_assets')
+    else:
+        form = AssetForm(instance=asset)
+    return render(request, 'plant/asset_form.html', {'form': form, 'title': 'Edit Asset'})
+
+def delete_asset(request, id):
+    asset = get_object_or_404(Asset, id=id)
+    if request.method == 'POST':
+        asset.delete()
+        return redirect('display_assets')
+    return render(request, 'plant/delete_asset.html', {'asset': asset})
+
+def display_parts(request):
+    parts = Part.objects.all().order_by('-id')
+    return render(request, 'plant/display_parts.html', {'parts': parts})
 
 def create_part(request):
     if request.method == 'POST':
         form = PartForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('create_setupfor')
+            return redirect('display_parts')
     else:
         form = PartForm()
     return render(request, 'plant/part_form.html', {'form': form, 'title': 'Add New Part'})
+
+def edit_part(request, id):
+    part = get_object_or_404(Part, id=id)
+    if request.method == 'POST':
+        form = PartForm(request.POST, instance=part)
+        if form.is_valid():
+            form.save()
+            return redirect('display_parts')
+    else:
+        form = PartForm(instance=part)
+    return render(request, 'plant/part_form.html', {'form': form, 'title': 'Edit Part'})
+
+def delete_part(request, id):
+    part = get_object_or_404(Part, id=id)
+    if request.method == 'POST':
+        part.delete()
+        return redirect('display_parts')
+    return render(request, 'plant/delete_part.html', {'part': part})

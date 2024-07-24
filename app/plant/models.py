1fe472e7ccbbe5ee0d28bdfae4b4a2862855f89f
Tyler Careless <tyler444c@gmail.com>
2024-07-24 13:17:46 -0400

    Migrated models. Going to get started on the crud page.

--- app/plant/models.py
+++ app/plant/models.py
@@ -1,3 +1,24 @@
+# plant/models.py
+
 from django.db import models
 
-# Create your models here.
+class Asset(models.Model):
+    asset_number = models.CharField(max_length=100)  
+
+    def __str__(self):
+        return self.asset_number
+
+class Part(models.Model):
+    part_number = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.part_number
+
+class SetupFor(models.Model):
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    part = models.ForeignKey(Part, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    since = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.asset.asset_number} setup for {self.part.part_number}'

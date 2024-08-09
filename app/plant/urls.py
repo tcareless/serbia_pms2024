2d1684f6b87fb350f2c529635340d82197395a52
Tyler Careless <tyler444c@gmail.com>
2024-08-09 09:23:45 -0400

    Moved into urlpatterns

--- app/plant/urls.py
+++ app/plant/urls.py
@@ -24,11 +24,7 @@ urlpatterns = [
     path('parts/delete/<int:id>/', delete_part, name='delete_part'),
     path('api/fetch_part_for_asset/', fetch_part_for_asset, name='fetch_part_for_asset'),
 
-]
-
-
-# passwords/urls.py
-urlpatterns = [
+    # passwords/urls.py
     path('password_list', password_list, name='password_list'),
     path('new/', password_create, name='password_create'),
     path('edit/<int:pk>/', password_edit, name='password_edit'),
@@ -38,3 +34,16 @@ urlpatterns = [
     path('auth/', auth_page, name='auth_page'),
 
 ]
+
+
+# # passwords/urls.py
+# urlpatterns = [
+#     path('password_list', password_list, name='password_list'),
+#     path('new/', password_create, name='password_create'),
+#     path('edit/<int:pk>/', password_edit, name='password_edit'),
+#     path('delete/<int:pk>/', password_delete, name='password_delete'),
+#     path('recover/<int:pk>/', password_recover, name='password_recover'),
+#     path('deleted/', deleted_passwords, name='deleted_passwords'),
+#     path('auth/', auth_page, name='auth_page'),
+
+# ]

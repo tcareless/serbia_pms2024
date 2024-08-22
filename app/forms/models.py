9922cf6b450b904af423e87440dabb6bfc50d828
Tyler Careless <tyler444c@gmail.com>
2024-08-22 10:45:09 -0400

    Created the models, now going to test some dummy forms

--- app/forms/models.py
+++ app/forms/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class FormSubmission(models.Model):
+    payload = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    form_type = models.ForeignKey('FormType', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Submission {self.id} - {self.form_type.name}"
+
+
+class FormType(models.Model):
+    name = models.CharField(max_length=255)
+    template_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name

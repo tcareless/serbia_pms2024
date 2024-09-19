b8477fcdc073628307dd2ef76c1f71174122b404
Tyler Careless <tyler444c@gmail.com>
2024-09-19 08:36:58 -0400

    one step further.

--- app/emailer/tasks.py
+++ app/emailer/tasks.py
@@ -3,11 +3,11 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 @shared_task
-def send_email_task(to_emails, subject, message, html_message):
+def send_email_task(to_emails, subject, message, html_message, from_email):
     send_mail(
         subject,
-        message,  # Plain text version of the message
-        settings.DEFAULT_FROM_EMAIL,
+        message,  # Plain text version of the message (can be an empty string)
+        from_email,  # Use the provided sender email
         to_emails,
         html_message=html_message  # HTML version of the message
     )

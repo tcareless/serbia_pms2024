8dbb4e5fbdd8e5e18145268636dcbcc7190061d8
Tyler Careless <tyler444c@gmail.com>
2024-10-10 13:01:17 -0400

    It's workinggggg :) Now need to test with some other form types and I need to clean up this page a ton and make it work fully.

--- app/forms/forms.py
+++ app/forms/forms.py
@@ -1,51 +1,23 @@
 from django import forms
 from .models import Form, FormQuestion, FormType
 
-# OIS FormType specific form
+# Example of OIS FormType specific form
 class OISForm(forms.ModelForm):
-    part_number = forms.CharField(
-        max_length=255,
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    operation = forms.CharField(
-        max_length=255,
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    part_name = forms.CharField(
-        max_length=255,
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    year = forms.CharField(
-        max_length=4,
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    mod_level = forms.CharField(
-        max_length=255,
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    machine = forms.CharField(
-        max_length=255,
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    mod_date = forms.DateField(
-        required=True,
-        widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'})
-    )
+    part_number = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    operation = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    part_name = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    year = forms.CharField(max_length=4, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    mod_level = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    machine = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    mod_date = forms.DateField(required=True, widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
 
     class Meta:
         model = Form
-        fields = ['name']  # Only the field(s) present in the Form model
+        fields = ['name']
 
     def save(self, commit=True):
         form_instance = super().save(commit=False)
-        form_type = FormType.objects.get(name="OIS")
-        form_instance.form_type = form_type
+        form_instance.form_type = FormType.objects.get(name="OIS")
         form_instance.metadata = {
             'part_number': self.cleaned_data['part_number'],
             'operation': self.cleaned_data['operation'],
@@ -60,56 +32,30 @@ class OISForm(forms.ModelForm):
         return form_instance
 
 
+# Example of OIS Question form
 class OISQuestionForm(forms.ModelForm):
-    question = forms.CharField(
-        max_length=255,
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    feature = forms.CharField(
-        max_length=255,
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    special_characteristic = forms.CharField(
-        max_length=255,
-        required=False,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    characteristic = forms.CharField(
-        max_length=255,
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    specifications = forms.CharField(
-        max_length=255,
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    sample_frequency = forms.CharField(
-        max_length=255,
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    sample_size = forms.CharField(
-        max_length=255,
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    done_by = forms.CharField(
-        max_length=255,
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
+    question = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    feature = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    special_characteristic = forms.CharField(max_length=255, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    characteristic = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    specifications = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    sample_frequency = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    sample_size = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    done_by = forms.CharField(max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = FormQuestion
         fields = ['question', 'feature', 'special_characteristic', 'characteristic', 'specifications', 'sample_frequency', 'sample_size', 'done_by']
 
 
-# Mapping of form type names to form classes
+# Dictionary for dynamically mapping form types to form classes
 FORM_TYPE_FORMS = {
     'OIS': OISForm,
-    # Add additional form types here as needed, e.g.:
-    # 'XYZForm': XYZFormClass
+    # Add additional form types here
+}
+
+# Dictionary for dynamically mapping form types to question form classes
+QUESTION_FORM_CLASSES = {
+    'OIS': OISQuestionForm,
+    # Add additional form types here
 }
--- app/forms/views.py
+++ app/forms/views.py
@@ -33,8 +33,124 @@ class FormTypeDeleteView(DeleteView):
     success_url = reverse_lazy('formtype_list')
 
 
+
+
+# View to create form and its questions
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import FORM_TYPE_FORMS, QUESTION_FORM_CLASSES
+from .models import FormType, FormQuestion
+from django.forms import modelformset_factory
+
 # View to create form and its questions
 def form_create_view(request):
+    form_type_id = request.GET.get('form_type')
+
+    if form_type_id:
+        form_type = get_object_or_404(FormType, id=form_type_id)
+        
+        # Dynamically get the form class for the form type
+        form_class = FORM_TYPE_FORMS.get(form_type.name)
+        question_form_class = QUESTION_FORM_CLASSES.get(form_type.name)
+
+        if form_class is None or question_form_class is None:
+            return render(request, 'forms/error.html', {'message': 'Form type not supported.'})
+
+        # Dynamically get fields for the question formset
+        question_fields = question_form_class._meta.fields
+
+        # Create a dynamic formset for questions with the fields from the question form
+        QuestionFormSet = modelformset_factory(FormQuestion, form=question_form_class, fields=question_fields, extra=1, can_delete=True)
+
+        if request.method == 'POST':
+            form = form_class(request.POST)
+            question_formset = QuestionFormSet(request.POST)
+
+            if form.is_valid() and question_formset.is_valid():
+                form_instance = form.save()
+
+                # Save questions dynamically
+                for question_form in question_formset:
+                    if question_form.cleaned_data and not question_form.cleaned_data.get('DELETE', False):
+                        # Dynamically capture all fields from the form, excluding 'id'
+                        question_data = {field: question_form.cleaned_data[field] for field in question_form.cleaned_data if field != 'id' and field != 'DELETE'}
+
+                        # Create and save FormQuestion instance
+                        question = FormQuestion(
+                            form=form_instance,
+                            question=question_data  # Store the dictionary in JSONField
+                        )
+                        question.save()
+
+                return redirect('form_create')  # Redirect after successful creation
+
+        else:
+            form = form_class()
+            question_formset = QuestionFormSet(queryset=FormQuestion.objects.none())
+
+        return render(request, 'forms/form_create.html', {
+            'form': form,
+            'question_formset': question_formset,
+            'form_type': form_type
+        })
+
+    # If no form_type is selected, show form type selection page
+    form_types = FormType.objects.all()
+    return render(request, 'forms/select_form_type.html', {'form_types': form_types})
+
+    form_type_id = request.GET.get('form_type')
+
+    if form_type_id:
+        form_type = get_object_or_404(FormType, id=form_type_id)
+        
+        # Dynamically get the form class for the form type
+        form_class = FORM_TYPE_FORMS.get(form_type.name)
+        question_form_class = QUESTION_FORM_CLASSES.get(form_type.name)
+
+        if form_class is None or question_form_class is None:
+            return render(request, 'forms/error.html', {'message': 'Form type not supported.'})
+
+        # Dynamically get fields for the question formset
+        question_fields = question_form_class._meta.fields
+
+        # Create a dynamic formset for questions with the fields from the question form
+        QuestionFormSet = modelformset_factory(FormQuestion, form=question_form_class, fields=question_fields, extra=1, can_delete=True)
+
+        if request.method == 'POST':
+            form = form_class(request.POST)
+            question_formset = QuestionFormSet(request.POST)
+
+            if form.is_valid() and question_formset.is_valid():
+                form_instance = form.save()
+
+                # Save questions dynamically
+                for question_form in question_formset:
+                    if question_form.cleaned_data and not question_form.cleaned_data.get('DELETE', False):
+                        # Dynamically capture all fields from the form
+                        question_data = {field: question_form.cleaned_data[field] for field in question_form.cleaned_data if field != 'DELETE'}
+
+                        # Create and save FormQuestion instance
+                        question = FormQuestion(
+                            form=form_instance,
+                            question=question_data  # Store the dictionary in JSONField
+                        )
+                        question.save()
+
+                return redirect('form_create')  # Redirect after successful creation
+
+        else:
+            form = form_class()
+            question_formset = QuestionFormSet(queryset=FormQuestion.objects.none())
+
+        return render(request, 'forms/form_create.html', {
+            'form': form,
+            'question_formset': question_formset,
+            'form_type': form_type
+        })
+
+    # If no form_type is selected, show form type selection page
+    form_types = FormType.objects.all()
+    return render(request, 'forms/select_form_type.html', {'form_types': form_types})
+
     form_type_id = request.GET.get('form_type')
 
     if form_type_id:
@@ -44,35 +160,75 @@ def form_create_view(request):
         if form_class is None:
             return render(request, 'forms/error.html', {'message': 'Form type not supported.'})
 
-        # Create a formset for the questions
-        QuestionFormSet = modelformset_factory(FormQuestion, form=OISQuestionForm, extra=1, can_delete=True)
+        # Create a dynamic formset for questions
+        QuestionFormSet = modelformset_factory(FormQuestion, form=OISQuestionForm, fields=['question', 'feature', 'special_characteristic', 'characteristic', 'specifications', 'sample_frequency', 'sample_size', 'done_by'], extra=1, can_delete=True)
 
         if request.method == 'POST':
             form = form_class(request.POST)
-            question_formset = QuestionFormSet(request.POST, queryset=FormQuestion.objects.none())
+            question_formset = QuestionFormSet(request.POST)
 
             if form.is_valid() and question_formset.is_valid():
                 form_instance = form.save()
 
-                # Save questions linked to the form
+                # Save questions dynamically
                 for question_form in question_formset:
                     if question_form.cleaned_data and not question_form.cleaned_data.get('DELETE', False):
-                        question_data = {
-                            "Done by": question_form.cleaned_data.get('done_by'),
-                            "Feature": question_form.cleaned_data.get('feature'),
-                            "Sample Size": question_form.cleaned_data.get('sample_size'),
-                            "Characteristic": question_form.cleaned_data.get('characteristic'),
-                            "Special Characteristic": question_form.cleaned_data.get('special_characteristic'),
-                            "Specifications": question_form.cleaned_data.get('specifications'),
-                            "Sample Frequency": question_form.cleaned_data.get('sample_frequency')
-                        }
+                        # Dynamically capture all fields from the form
+                        question_data = {field: question_form.cleaned_data[field] for field in question_form.cleaned_data if field != 'DELETE'}
 
-                        # Create a new FormQuestion instance and link it to the form
+                        # Create and save FormQuestion instance
                         question = FormQuestion(
                             form=form_instance,
                             question=question_data  # Store the dictionary in JSONField
                         )
-                        question.save()  # Save the question to the database
+                        question.save()
+
+                return redirect('form_create')  # Redirect after successful creation
+
+        else:
+            form = form_class()
+            question_formset = QuestionFormSet(queryset=FormQuestion.objects.none())
+
+        return render(request, 'forms/form_create.html', {
+            'form': form,
+            'question_formset': question_formset,
+            'form_type': form_type
+        })
+
+    # If no form_type is selected, show form type selection page
+    form_types = FormType.objects.all()
+    return render(request, 'forms/select_form_type.html', {'form_types': form_types})
+    form_type_id = request.GET.get('form_type')
+
+    if form_type_id:
+        form_type = get_object_or_404(FormType, id=form_type_id)
+        form_class = FORM_TYPE_FORMS.get(form_type.name)
+
+        if form_class is None:
+            return render(request, 'forms/error.html', {'message': 'Form type not supported.'})
+
+        # Create a dynamic formset for questions
+        QuestionFormSet = modelformset_factory(FormQuestion, extra=1, can_delete=True)
+
+        if request.method == 'POST':
+            form = form_class(request.POST)
+            question_formset = QuestionFormSet(request.POST)
+
+            if form.is_valid() and question_formset.is_valid():
+                form_instance = form.save()
+
+                # Save questions dynamically, no hardcoding of fields
+                for question_form in question_formset:
+                    if question_form.cleaned_data and not question_form.cleaned_data.get('DELETE', False):
+                        # Dynamically capture all fields from the form
+                        question_data = {field: question_form.cleaned_data[field] for field in question_form.cleaned_data if field != 'DELETE'}
+
+                        # Create and save FormQuestion instance
+                        question = FormQuestion(
+                            form=form_instance,
+                            question=question_data  # Store the dictionary in JSONField
+                        )
+                        question.save()
 
                 return redirect('form_create')  # Redirect after successful creation
 

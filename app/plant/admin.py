b91cdfd1d85dd463d0877be18d7277486d41cc51
Tyler Careless <tyler444c@gmail.com>
2024-11-12 15:06:37 -0500

    Registered models to admin.py

--- app/plant/admin.py
+++ app/plant/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models.setupfor_models import Asset, Part, SetupFor
 
-# Register your models here.
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ('asset_number', 'asset_name')
+    search_fields = ('asset_number', 'asset_name')
+
+
+@admin.register(Part)
+class PartAdmin(admin.ModelAdmin):
+    list_display = ('part_number', 'part_name')
+    search_fields = ('part_number', 'part_name')
+
+
+@admin.register(SetupFor)
+class SetupForAdmin(admin.ModelAdmin):
+    list_display = ('asset', 'part', 'created_at', 'since')
+    search_fields = ('asset__asset_number', 'part__part_number')
+    list_filter = ('created_at', 'since')
+    ordering = ('-created_at',)

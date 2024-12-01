596444b34df73698a593d0516290c042f6b219b8
Chris Strutton <chris@rodandfly.ca>
2024-12-01 13:32:30 -0500

    changed name of running container to pms-stage for testing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,8 +7,8 @@ git pull
 
 docker build -t pms:$version .
 
-docker container rm --force pms &>/dev/null && echo 'Removed old container'
+docker container rm --force pms-stage &>/dev/null && echo 'Removed old container'
 
 # docker run -d -p 80:80 --name pms --restart=unless-stopped pms:$version
 
-docker run -d -p 8089:80 --name pms --restart=unless-stopped -v /var/pms/media:/app/media_files pms:$version
+docker run -d -p 8089:80 --name pms-stage --restart=unless-stopped -v /var/pms/media:/app/media_files pms:$version

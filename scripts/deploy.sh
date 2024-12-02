3d65faedb332ab840c7360dbac29c8428b464c48
Chris Strutton <chris@rodandfly.ca>
2024-12-02 08:32:52 -0500

    pushed version number

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 set -e
-version="0.2.6"
+version="0.2.7"
 
-git checkout mediafiles
+git checkout production
 
 git pull
 
@@ -9,6 +9,4 @@ docker build -t pms:$version .
 
 docker container rm --force pms &>/dev/null && echo 'Removed old container'
 
-# docker run -d -p 80:80 --name pms --restart=unless-stopped pms:$version
-
-docker run -d -p 8089:80 --name pms --restart=unless-stopped -v /var/pms/media:/app/media_files pms:$version
+docker run -d -p 80:80 --name pms --restart=unless-stopped -v /var/pms/media:/app/media_files pms:$version

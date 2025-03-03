019c9b1083a22fd92baa1530d5cc7b7abc24c967
Tyler Careless <tyler444c@gmail.com>
2025-03-03 14:05:32 -0500

    .

--- app/forms/tasks.py
+++ app/forms/tasks.py
@@ -4,6 +4,9 @@ import time
 @shared_task
 def long_running_task():
     """Simulate a long-running task."""
-    for i in range(5):
-        time.sleep(2)  # Simulating work
+    for i in range(3):
+        time.sleep(1)  # Simulating work
     return "Task Complete"
+
+
+# celery -A pms worker --loglevel=info

bb976ae577567fa880c95ea80ea3bd6993cbd835
Tyler Careless <tyler444c@gmail.com>
2025-03-05 10:30:18 -0500

    .

--- app/prod_query/testing_press_runtime.py
+++ app/prod_query/testing_press_runtime.py
@@ -24,25 +24,6 @@ The target each week is 27,496 parts made
 
 
 
-'''
-Press 272
-
-Feb 2 11pm (1738555200) until Feb 7th 11pm (1738987200)
-
-We calculated 2011 minutes of total downtime where 509 of those minutes were planned.
-
-
-There is a total of 7200 total potential minutes in a week
-
-
-We produced 16,873 parts in that time from GFx table directly
-
-
-The target each week is 27,496 parts made
-'''
-
-
-
 # Data for Press 272 (all times in minutes)
 total_potential_minutes = 7200      # Total potential minutes in a week
 planned_downtime = 509              # Planned downtime (minutes)

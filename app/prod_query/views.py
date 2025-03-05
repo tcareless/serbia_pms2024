bbdbb517644ac63a998a61dcfb5e1bd2c69efd96
Tyler Careless <tyler444c@gmail.com>
2025-03-05 09:07:27 -0500

    selecting idnumber now in prdowntime table

--- app/prod_query/views.py
+++ app/prod_query/views.py
@@ -4416,7 +4416,7 @@ def fetch_press_prdowntime1_entries(assetnum, called4helptime, completedtime):
 
         # Raw SQL query to fetch the required data
         query = """
-        SELECT problem, called4helptime, completedtime
+        SELECT idnumber, problem, called4helptime, completedtime
         FROM pr_downtime1
         WHERE assetnum = %s
         AND down = 'Yes_Down'

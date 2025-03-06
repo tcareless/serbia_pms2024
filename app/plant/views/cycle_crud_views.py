81ca3ea9e947af2c5bf861c8f4eae679afd96851
Tyler Careless <tyler444c@gmail.com>
2025-03-06 13:37:17 -0500

    Now converts to epoch, nice

--- app/plant/views/cycle_crud_views.py
+++ app/plant/views/cycle_crud_views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from ..forms.cycle_crud_forms import AssetCycleTimeForm
+import time
+
 
 def asset_cycle_times_page(request):
     if request.method == 'POST':
         form = AssetCycleTimeForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
+            epoch_timestamp = int(data['datetime'].timestamp())  # Convert to epoch
+
             print("Asset:", data['asset'])
             print("Part:", data['part'])
             print("Cycle Time:", data['cycle_time'])
-            print("Date & Time:", data['datetime'])
+            print("Effective Date (Epoch Timestamp):", epoch_timestamp)  # Print epoch timestamp
+
     else:
         form = AssetCycleTimeForm()
 
     return render(request, 'asset_cycle_times.html', {'form': form})
+

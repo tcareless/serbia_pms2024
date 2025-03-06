3484bbac64be7c3ecf4f8a11347a79233e4ba2ff
Tyler Careless <tyler444c@gmail.com>
2025-03-06 13:35:32 -0500

    Now pullnig from part and asset table, nice

--- app/plant/forms/cycle_crud_forms.py
+++ app/plant/forms/cycle_crud_forms.py
@@ -1,19 +1,8 @@
 from django import forms
+from ..models.setupfor_models import Asset, Part
 
 class AssetCycleTimeForm(forms.Form):
-    ASSET_CHOICES = [
-        ('asset_1', 'Asset 1'),
-        ('asset_2', 'Asset 2'),
-        ('asset_3', 'Asset 3'),
-    ]
-    
-    PART_CHOICES = [
-        ('part_1', 'Part 1'),
-        ('part_2', 'Part 2'),
-        ('part_3', 'Part 3'),
-    ]
-
-    asset = forms.ChoiceField(choices=ASSET_CHOICES, label="Select Asset")
-    part = forms.ChoiceField(choices=PART_CHOICES, label="Select Part")
+    asset = forms.ModelChoiceField(queryset=Asset.objects.all(), label="Select Asset")
+    part = forms.ModelChoiceField(queryset=Part.objects.all(), label="Select Part")
     cycle_time = forms.FloatField(label="Cycle Time", min_value=0)
     datetime = forms.DateTimeField(widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}), label="Date & Time")

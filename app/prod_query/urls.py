c2c5b38aba9f552e6ef71cd5b24f9f4c40986c16
Tyler Careless <tyler444c@gmail.com>
2025-03-13 10:09:54 -0400

    .

--- app/prod_query/urls.py
+++ app/prod_query/urls.py
@@ -47,8 +47,8 @@ urlpatterns = [
     path('downtime-frequency/', views.downtime_frequency_view, name='downtime_frequency'),
 
 
-    path('press_runtime/', views.press_runtime_wrapper, name='press_runtime'),
-    path('press_runtime2/', views.press_runtime_wrapper2, name='press_runtime2'),
+    path('press_runtime_details/', views.press_runtime_wrapper, name='press_runtime'),
+    path('press_runtime/', views.press_runtime_wrapper2, name='press_runtime2'),
 
 
 
